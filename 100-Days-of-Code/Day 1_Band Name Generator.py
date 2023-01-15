c3f01c3e08f037148475e0b31d90b66f5ad39301
Steven Roscoe <dbzroscoe@gmail.com>
2023-01-14 18:49:43 -0600

    Day 1

--- 100-Days-of-Code/Day 1_Band Name Generator.py
+++ 100-Days-of-Code/Day 1_Band Name Generator.py
@@ -1,10 +1,8 @@
 #Print greeting for the name generator
-print("Welcome to the Band Name Generator!\n")
+print("Welcome to the Band Name Generator!")
 #Ask user for their city input
 city = input("What city did you grow up in?\n")
 #Ask user for their pet input
 pet = input("What is the name of your pet?\n")
-#Print the string
-print("Your new band name is the:\n")
 #Print the band name 
-print(city + " " + pet)
+print('Your new band name is ' + city + " " + pet)
